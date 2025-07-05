c9434d047694b70048864d51afb0ce247bdeec77
NoshinUlfat <ulfat.noshin.007@gmail.com>
2025-07-05 23:58:57 +0600

    app-routing changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomePageComponent } from "./modules/home-page/home-page.component";
-import { TaskAnnouncementComponent } from "./modules/task-announcement/task-announcement.component";
-import { StartersComponent } from "./modules/starters/starters.component";
-import { ImportantDatesComponent } from "./modules/important-dates/important-dates.component";
+// app-routing.module.ts
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import { TaskAnnouncementComponent } from './modules/task-announcement/task-announcement.component';
+import { StartersComponent } from './modules/starters/starters.component';
+import { ImportantDatesComponent } from './modules/important-dates/important-dates.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'home', component: HomePageComponent},
-  {path: 'task-announcement', component: TaskAnnouncementComponent},
-  {path: 'get-started', component: StartersComponent},
-  {path: 'important-dates', component: ImportantDatesComponent},
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'task‑announcement', component: TaskAnnouncementComponent },
+  { path: 'get‑started', component: StartersComponent },
+  { path: 'important‑dates', component: ImportantDatesComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
+  exports: [ RouterModule ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
