3f8ec3538aefd59e7f233b8f6acd6400037fc155
NoshinUlfat <ulfat.noshin.007@gmail.com>
2025-08-04 23:43:52 +0600

    task announcement and starter kit modifications

--- src/app/modules/starters/starters.module.ts
+++ src/app/modules/starters/starters.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartersComponent } from './starters.component';
+import { EvaluationAndSubmissionComponent } from './components/evaluation-and-submission/evaluation-and-submission.component';
+import { StepByStepWorkflowComponent } from './components/step-by-step-workflow/step-by-step-workflow.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StartersComponent],
-  exports: [StartersComponent],
+  declarations: [StartersComponent, EvaluationAndSubmissionComponent, StepByStepWorkflowComponent,],
+  exports: [StartersComponent, EvaluationAndSubmissionComponent, StepByStepWorkflowComponent,],
 })
 export class StartersModule { }
--- src/app/modules/task-announcement/task-announcement.module.ts
+++ src/app/modules/task-announcement/task-announcement.module.ts
@@ -4,14 +4,12 @@ import { TaskAnnouncementComponent } from './task-announcement.component';
 import { LayoutModule } from '../layout/layout.module';
 import { TaskDescriptionComponent } from './components/task-description/task-description.component';
 import { DatasetsComponent } from './components/datasets/datasets.component';
-import { StepByStepWorkflowComponent } from './components/step-by-step-workflow/step-by-step-workflow.component';
-import { EvaluationAndSubmissionComponent } from './components/evaluation-and-submission/evaluation-and-submission.component';
 
 @NgModule({
   imports: [
     CommonModule, LayoutModule
   ],
-  declarations: [DatasetsComponent, EvaluationAndSubmissionComponent, StepByStepWorkflowComponent, TaskAnnouncementComponent, TaskDescriptionComponent],
-  exports: [DatasetsComponent, EvaluationAndSubmissionComponent, StepByStepWorkflowComponent, TaskAnnouncementComponent, TaskDescriptionComponent],
+  declarations: [DatasetsComponent, TaskAnnouncementComponent, TaskDescriptionComponent],
+  exports: [DatasetsComponent, TaskAnnouncementComponent, TaskDescriptionComponent],
 })
 export class TaskAnnouncementModule { }
