25a7771829c3b00e51730b94322eae416a09cfc1
NoshinUlfat <ulfat.noshin.007@gmail.com>
2025-08-11 01:53:06 +0600

    updated before publishing

--- src/app/modules/home-page/components/events/events.component.ts
+++ src/app/modules/home-page/components/events/events.component.ts
@@ -11,14 +11,14 @@ export class EventsComponent {
   layout: 'horizontal' | 'vertical' = 'horizontal';
 
   events: TimelineEvent[] = [
-    { date: '10 Jul, 2025', title: 'Registration on codalab and beginning of the development cycle', color: '#6fa8dc' },
-    { date: '10-31 Aug, 2025', title: 'Dev Phase', color: '#e06666' },
-    { date: '1-7 Sep, 2025', title: 'Test Phase', color: '#93c47d' },
-    { date: '10 Sep, 2025', title: 'Final Ranking Published & Paper Invitations', color: '#6fa8dc' },
-    { date: '29 Sep, 2025', title: 'Paper Submission Deadline (Shared Task System Papers Due)', color: '#e06666' },
+    // { date: '10 Jul, 2025', title: 'Registration on codalab and beginning of the development cycle', color: '#6fa8dc' },
+    { date: '10 Aug - 7 Sep, 2025', title: 'Dev Phase', color: '#e06666' },
+    { date: '8-14 Sep, 2025', title: 'Test Phase', color: '#93c47d' },
+    { date: '15 Sep, 2025', title: 'Final Ranking Published & Paper Invitations', color: '#6fa8dc' },
+    { date: '29 Sep, 2025', title: 'Paper Submission Deadline', color: '#e06666' },
     { date: '3 Nov, 2025', title: 'Notification of acceptance', color: '#93c47d' },
     { date: '11 Nov, 2025', title: 'Camera‑ready Due', color: '#6fa8dc' },
-    { date: '23 Dec, 2025', title: 'Workshop co-located with IJCNLP-AACL 2025 (Mumbai, India)', color: '#e06666' },
+    { date: '23 Dec, 2025', title: 'Workshop co-located with IJCNLP-AACL 2025', color: '#e06666' },
   ];
   
   constructor() {
