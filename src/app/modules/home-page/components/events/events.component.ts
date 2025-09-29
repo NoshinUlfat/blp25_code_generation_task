eaad4e97e4c1179aad561bc98bbd57a63e99038e
NoshinUlfat <ulfat.noshin.007@gmail.com>
2025-09-29 21:24:52 +0600

    paper submission deadline updated

--- src/app/modules/home-page/components/events/events.component.ts
+++ src/app/modules/home-page/components/events/events.component.ts
@@ -15,7 +15,7 @@ export class EventsComponent {
     { date: '10 Aug - 7 Sep, 2025', title: 'Dev Phase', color: '#e06666' },
     { date: '8-14 Sep, 2025', title: 'Test Phase', color: '#93c47d' },
     { date: '15 Sep, 2025', title: 'Final Ranking Published & Paper Invitations', color: '#6fa8dc' },
-    { date: '29 Sep, 2025', title: 'Paper Submission Deadline', color: '#e06666' },
+    { date: '4 Oct, 2025', title: 'Paper Submission Deadline', color: '#e06666' },
     { date: '3 Nov, 2025', title: 'Notification of acceptance', color: '#93c47d' },
     { date: '11 Nov, 2025', title: 'Camera‑ready Due', color: '#6fa8dc' },
     { date: '23 Dec, 2025', title: 'Workshop co-located with IJCNLP-AACL 2025', color: '#e06666' },
